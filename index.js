156823aa77a40a9a8c78e17fbfc71201e2fa91e3
AYBERK ANIL ATSIZ <anil@monkeysteam.com>
2020-10-30 17:57:02 +0300

    fix

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const fetch = require('isomorphic-fetch')
-​
 const defaultHeaders = {
   'x-ig-capabilities': '3w==',
   'user-agent': 'Instagram 9.5.1 (iPhone9,2; iOS 10_0_2; en_US; en-US; scale=2.61; 1080x1920) AppleWebKit/420+',
   host: 'i.instagram.com'
 }
-​
+
 const getHeaders = (headers, sessionid, userid) => {
   return Object.assign(headers, {
     cookie: `sessionid=${sessionid}; ds_user_id=${userid}`
